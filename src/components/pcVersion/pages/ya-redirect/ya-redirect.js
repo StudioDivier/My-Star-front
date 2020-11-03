bf93d9da529fb8cf801d6b35e13899dfdabc9d31
Gleb <gleb.rezun@gmail.com>
2020-11-03 10:27:39 +0300

    yaOauth

--- src/components/pcVersion/pages/ya-redirect/ya-redirect.js
+++ src/components/pcVersion/pages/ya-redirect/ya-redirect.js
@@ -13,18 +13,27 @@ export const YaRedirect = ({phone}) => {
     const history = useHistory();
     const {request} = useHttp();
 
+    const storageName = 'tempUserData';
+    const tempUserData = JSON.parse(window.localStorage.getItem(storageName));
+
     useEffect(() => {
         // try {
         async function fetchData() {
             const dataAuth = await request(`/api/mid-yandex/?code=${code}`, 'GET')
+
             console.log(dataAuth)
+            localStorage.setItem(storageName, JSON.stringify({
+                access_token: dataAuth.access_token, expires_in: dataAuth.expires_in, refresh_token: dataAuth.refresh_token
+            }))
 
             const dataSend = await request(`/api/yandex-oauth/`, 'POST', {
-                access_token: dataAuth.access_token,
-                expires_in: dataAuth.expires_in,
-                refresh_token: dataAuth.refresh_token
+                access_token: tempUserData.access_token,
+                expires_in: tempUserData.expires_in,
+                refresh_token: tempUserData.refresh_token
             })
+
             console.log(dataSend)
+            localStorage.removeItem(storageName)
 
             const dataLog = await request('/api/login/', 'POST', {
                 id: dataSend.id,
@@ -39,6 +48,7 @@ export const YaRedirect = ({phone}) => {
             auth.login(dataLog.token, dataLog.username, dataLog.is_star, dataLog.id);
             history.push('/')
         }
+
         //
         // } catch (e) {
         //     message(e);
