16b092ac3dc96ea0db3b172f786d55830308954a
Gleb <gleb.rezun@gmail.com>
2020-11-05 18:48:28 +0300

    qjhwerqjh

--- src/components/see-video/SeeVideo.js
+++ src/components/see-video/SeeVideo.js
@@ -8,15 +8,19 @@ export const SeeVideo = () => {
 
     return (
         <>
-            <iframe
-                width="560"
-                height="405"
-                src={`${window.location.href}`}
-                frameBorder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-                title="Congratulation video"
-                allowFullScreen>
-            </iframe>
+            {/*<iframe*/}
+            {/*    width="560"*/}
+            {/*    height="405"*/}
+            {/*    src={`${window.location.href}`}*/}
+            {/*    frameBorder="0"*/}
+            {/*    allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"*/}
+            {/*    title="Congratulation video"*/}
+            {/*    allowFullScreen>*/}
+            {/*</iframe>*/}
+            <video width="560" height="405" controls>
+                <source src="http://exprome.ru:8080/media/congratulation/2/c29c5f1b-4175-4ee0-bdfc-acfa8ac02e40.mp4"
+                        type="video/mp4" />
+            </video>
         </>
-    )
+)
 }
